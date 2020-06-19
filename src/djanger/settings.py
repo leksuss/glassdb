a844da1b40a50ba93276d9df27d87bbfe90c9db1
Andrey <leksus@leksus.info>
2020-06-19 20:31:20 +0300

    fixed wrong using collectstatic command

--- src/djanger/settings.py
+++ src/djanger/settings.py
@@ -74,23 +74,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
-# https://docs.djangoproject.com/en/3.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
 MEDIA_ROOT = public_root('media')
